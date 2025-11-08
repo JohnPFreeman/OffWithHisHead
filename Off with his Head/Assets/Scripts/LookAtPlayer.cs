a1aea19daacc6072a795888609535af6137a7957
alexchang319 <alexchang319@outlook.com>
2025-11-08 13:03:23 -0800

    pushtofetch

--- Off with his Head/Assets/Scripts/LookAtPlayer.cs
+++ Off with his Head/Assets/Scripts/LookAtPlayer.cs
@@ -1,13 +1,17 @@
 using System.Numerics;
-using System.Threading.Tasks.Dataflow;
 using JetBrains.Annotations;
 using Microsoft.VisualBasic;
+using UnityEditor.UI;
 using UnityEngine;
 
 public class LookAtPlayer
 {
-        void Start()
+    void Start()
     {
         //int x = Input.
+    }
+    void Update()
+    {
+        //gameObject.
     }
 }
