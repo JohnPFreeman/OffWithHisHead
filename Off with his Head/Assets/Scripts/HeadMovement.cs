0f5a9c2bdcaf92b6b264cb31890b1588c44ba7ee
TheCode327 <118479404+TheCode327@users.noreply.github.com>
2025-11-08 11:39:16 -0800

    hi
    bye

--- Off with his Head/Assets/Scripts/HeadMovement.cs
+++ Off with his Head/Assets/Scripts/HeadMovement.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class HeadMovement : MonoBehaviour
 {
+    private Vector2 v;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+
+        // Update is called once per frame
+        void Update()
+        {
+            v = Vector3.zero;
+
+            if (Input.GetKey(KeyCode.W))
+            {
+                v += transform.forward;
+
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                v -= transform.right;
+
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                v -= transform.forward;
+
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                v += transform.right;
+            }
+        }
     }
 }
