91e361c4d9450cff37613c8709b0d0b571a51cf4
TheCode327 <118479404+TheCode327@users.noreply.github.com>
2025-11-08 15:01:29 -0800

    movement in progress

--- Off with his Head/Assets/Scripts/HeadMovement.cs
+++ Off with his Head/Assets/Scripts/HeadMovement.cs
@@ -11,6 +11,10 @@ public class HeadMovement : MonoBehaviour
     private Vector3 v;
 
     private bool headMode = false;
+    private bool xMovable = false;
+    private bool yMovable = false;
+
+    public GameObject sprite;
 
 
     // Start is called before the first frame update
@@ -23,34 +27,118 @@ public class HeadMovement : MonoBehaviour
     }
 
     // Update is called once per frame
+    private void Update()
+    {
+        updateRecentDir();
+    }
+
     void FixedUpdate()
     {
         v = Vector3.zero;
         headMode = gm.headMode;
 
+        movement();
+        
+    }
+
+    void movement()
+    {
 
         if (Input.GetKey(KeyCode.W))
         {
-            v += transform.forward;
+            v += new Vector3(0, 0, 1);
 
         }
-        if (Input.GetKey(KeyCode.A))
-        {
-            v -= transform.right;
 
-        }
         if (Input.GetKey(KeyCode.S))
         {
-            v -= transform.forward;
+            v -= new Vector3(0, 0, 1);
 
         }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            v -= new Vector3(1, 0, 0);
+
+        }
+
         if (Input.GetKey(KeyCode.D))
         {
-            v += transform.right;
+            v += new Vector3(1, 0, 0);
+
         }
 
-        v = v.normalized * moveSpeed * (headMode?1:0);
+        if (!yMovable)
+        {
+           v[2] = 0;
+        }
+        if (!xMovable)
+        {
+            v[0] = 0;
+        }
+
+        if (headMode)
+        {
+            updateRotation();
+        }
+       
+        
+        
+
+        v = v.normalized * moveSpeed * (headMode ? 1 : 0);
 
         transform.position += v;
+
+
+    }
+
+    void updateRecentDir()
+    {
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.D))
+        {
+            xMovable = true;
+            yMovable = false;
+            //Debug.Log(xMostRecent);
+        }
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.S))
+        {
+            xMovable = false;
+            yMovable = true;
+            //Debug.Log(xMostRecent);
+        }
+
+        if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D))
+        {
+            xMovable = false;
+            yMovable = true;
+            //Debug.Log(xMostRecent);
+        }
+        if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.S))
+        {
+            xMovable = true;
+            yMovable = false;
+            //Debug.Log(xMostRecent);
+        }
+    }
+
+    void updateRotation()
+    {
+        if (v == new Vector3(1, 0, 0))
+        {
+            sprite.transform.localEulerAngles = new Vector3(90, 90, 0);
+                
+        } else if (v == new Vector3(-1, 0, 0))
+        {
+            sprite.transform.localEulerAngles = new Vector3(90,-90, 0);
+
+        } else if (v == new Vector3(0, 0, 1))
+        {
+            sprite.transform.localEulerAngles = new Vector3(90, 0, 0);
+
+        } else if (v == new Vector3(0, 0, -1))
+        {
+            sprite.transform.localEulerAngles = new Vector3(90, 180, 0);
+
+        }
     }
 }
