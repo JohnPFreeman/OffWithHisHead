6e168a9d91e56c684134bc408148ed99d8f246da
alexchang319 <alexchang319@outlook.com>
2025-11-08 12:52:02 -0800

    nextstuff

---
+++ Off with his Head/Assets/Scripts/LookAtPlayer.cs
@@ -0,0 +1,13 @@
+using System.Numerics;
+using System.Threading.Tasks.Dataflow;
+using JetBrains.Annotations;
+using Microsoft.VisualBasic;
+using UnityEngine;
+
+public class LookAtPlayer
+{
+        void Start()
+    {
+        int x = Input.
+    }
+}
