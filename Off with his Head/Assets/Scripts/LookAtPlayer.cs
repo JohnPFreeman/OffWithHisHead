4498e065315c49688f7f24cf04066d05fb5f08cc
alexchang319 <alexchang319@outlook.com>
2025-11-08 16:22:52 -0800

    to pull

--- Off with his Head/Assets/Scripts/LookAtPlayer.cs
+++ Off with his Head/Assets/Scripts/LookAtPlayer.cs
@@ -21,21 +21,24 @@ public class LookAtPlayer : MonoBehaviour
         //direction.z = 0;
         //direction.w = 1;
         //transform.rotation = direction;
-        //if ((player.transform.position.z - transform.position.z) > 1 ^ (player.transform.position.x - transform.position.x) < 1)
-        //{
-        //    angle = (float)(Math.Atan((player.transform.position.x - transform.position.x) /
-        //                                (player.transform.position.z - transform.position.z)) * 360 / (2 * Math.PI) * (0 - 1));
-        //}
-        //else if ((player.transform.position.z - transform.position.z) < 1 ^ (player.transform.position.x - transform.position.x) > 1)
-        //{
-        //    angle = (float)(Math.Atan((player.transform.position.x - transform.position.x) /
-        //                                (player.transform.position.z - transform.position.z)) * 360 / (2 * Math.PI));
-        //}
+        if (player.transform.position.x >= transform.position.x)
+        {//} >= 1 ^ (player.transform.position.x - transform.position.x) < 1)
+            
+                angle = (float)Math.Atan(player.transform.position.z - transform.position.z /
+                                            (player.transform.position.x - transform.position.x)* 360 / (2 * Math.PI) * (1));
+            
+        }
+        else
+        {
+            // if ((player.transform.position.z - transform.position.z) < 1 ^ (player.transform.position.x - transform.position.x) > 1)
+            angle = (float)(180 + Math.Atan((transform.position.z-player.transform.position.z) /
+                                        (transform.position.x-player.transform.position.x )) * 360 / (2 * Math.PI));
+        }
         //else
         //{
         //    
         //}
-        transform.eulerAngles = new Vector3(90, 0, -Vector3.Angle(transform.position-player.transform.position,Vector3.forward));// LookAt(player.transform, Vector3.up))
+        transform.eulerAngles = new Vector3(90, 0, angle);// LookAt(player.transform, Vector3.up))
         //transform.position += Vector3.zero;
         //transform.LookAt(player.transform, Vector3.left);
         //transform.LookAt(player.transform.position);
